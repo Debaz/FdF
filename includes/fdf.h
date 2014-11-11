f5a286a8cecd59596150f36522036e6f257263b4
Kantin LESCAUDRON <klescaud@e2r13p14.42.fr>
2014-11-11 13:55:22 +0100

    tests divers

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: klescaud <klescaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/11 13:06:55 by klescaud          #+#    #+#             */
-/*   Updated: 2014/11/11 13:20:39 by klescaud         ###   ########.fr       */
+/*   Updated: 2014/11/11 13:54:50 by klescaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,20 @@
 # define SIZE_X 800
 # define SIZE_Y 600
 # define TITLE "Visualisateur Fil de Fer - by Debaz"
+# define ABS(x) ((( x ) < 0 ) ? - ( x ) : ( x ))
 
 typedef struct			s_screen
 {
 	void		*mlx_ptr;
 	void		*win_ptr;
 }						t_screen;
+
+typedef struct			s_point
+{
+	int		x;
+	int		y;
+}						t_point;
+
+void		draw_segment(t_screen screen, t_point a, t_point b);
 
 #endif
--- source/main.c
+++ source/main.c
@@ -6,13 +6,13 @@
 /*   By: klescaud <klescaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/11 12:38:00 by klescaud          #+#    #+#             */
-/*   Updated: 2014/11/11 13:28:30 by klescaud         ###   ########.fr       */
+/*   Updated: 2014/11/11 13:54:53 by klescaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	do_fdf(void)
+t_screen	do_fdf(void)
 {
 	t_screen	screen;
 
@@ -22,14 +22,29 @@ void	do_fdf(void)
 		ft_putendl("ERROR -> win_ptr == NULL.");
 	mlx_loop(screen.mlx_ptr);
 	ft_putendl("Tout va bien.");
+	return (screen);
+}
+
+void		draw_segment(t_screen screen, t_point a, t_point b)
+{
+	
 }
 
 int		main(int ac, char **av)
 {
+	t_screen	screen;
+	t_point		a;
+	t_point		b;
+
+	a.x = 100;
+	a.y = 100;
+	b.x = 200;
+	b.y = 200;
 	if (ac == 1)
 	{
 		av[0] = av[0];
-		do_fdf();
+		screen = do_fdf();
+		draw_segment(screen, a, b);
 	}
 	return (0);
 }
